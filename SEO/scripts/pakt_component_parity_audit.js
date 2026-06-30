fb327bcc202dfaf64b25b66d9ba0fb59a755154b
Ivan Provalov <iprovalov@netflix.com>
2026-06-30 07:14:11 -0700

    Removing SEO Content

--- SEO/scripts/pakt_component_parity_audit.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-process.env.PAGE_PATH = process.env.PAGE_PATH || "/pakt/component-parity/";
-process.env.REPORT_BASENAME = process.env.REPORT_BASENAME || "pakt_component_parity_audit";
-process.env.REPORT_TITLE = process.env.REPORT_TITLE || "Pakt Component Parity Visual Audit";
-process.env.SCREENSHOT_SUBDIR = process.env.SCREENSHOT_SUBDIR || "component-parity";
-
-require("./pakt_visual_parity_audit.js");
--- SEO/scripts/pakt_component_parity_qa.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-process.env.SHOWCASE_PATH = process.env.SHOWCASE_PATH || "/pakt/component-parity/";
-process.env.REPORT_BASENAME = process.env.REPORT_BASENAME || "pakt_component_parity_qa";
-process.env.REPORT_TITLE = process.env.REPORT_TITLE || "Pakt Component Parity QA";
-process.env.SCREENSHOT_SUBDIR = process.env.SCREENSHOT_SUBDIR || "component-parity-qa";
-
-require("./pakt_component_showcase_qa.js");
